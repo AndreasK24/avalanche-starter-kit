412a53b24003c385585f67ebb5b004384ac2eb6a
AndreasK24 <96496322+AndreasK24@users.noreply.github.com>
2024-10-20 10:51:41 +0000

    Update

--- contracts/MaGGA/createToken.sol
+++ contracts/MaGGA/createToken.sol
@@ -1,17 +1,12 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.18;
 
-import "@openzeppelin/contracts@5.0.2/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts@5.0.2/access/Ownable.sol";
-import "@openzeppelin/contracts@5.0.2/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20, Ownable, ERC20Permit {
-    constructor(address initialOwner)
-        ERC20("MyToken", "MTK")
-        Ownable(initialOwner)
-        ERC20Permit("MyToken")
-    {}
+contract MyToken is ERC20, Ownable {
+    constructor(address initialOwner) ERC20("MyToken", "MTK") Ownable() {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
